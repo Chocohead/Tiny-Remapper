dd3abf29c97535b0cfa129de30b5609514766b4a
Chocohead <Chocohead@Aroma1997.org>
2019-03-12 00:13:56 +0000

    Switch to remapped description
    Matches what Loom uses, not strictly necessary but makes the stores easier to read

--- src/main/java/net/fabricmc/tinyremapper/AsmClassRemapper.java
+++ src/main/java/net/fabricmc/tinyremapper/AsmClassRemapper.java
@@ -44,7 +44,7 @@ class AsmClassRemapper extends ClassRemapper {
 		String remappedDescriptor = remapper.mapMethodDesc(descriptor);
 		String mappedName = remapper.mapMethodName(className, name, descriptor);
 		MethodVisitor methodVisitor = cv.visitMethod(access, mappedName, remappedDescriptor, remapper.mapSignature(signature, false), exceptions == null ? null : remapper.mapTypes(exceptions));
-		String[] locals = ((AsmRemapper) remapper).getLocalVariables(remapper.mapType(className), mappedName, descriptor);
+		String[] locals = ((AsmRemapper) remapper).getLocalVariables(remapper.mapType(className), mappedName, remappedDescriptor);
 		return new AsmMethodRemapper(methodVisitor, remapper, locals, renameInvalidLocals);
 	}
 
