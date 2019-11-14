2be4d64917f773a262340095233bb68cce12d273
Chocohead <Chocohead@Aroma1997.org>
2019-11-14 23:45:27 +0000

    Add Tiny V2 support
    Integrates parts of FabricMC/tiny-remapper@ca896ec, FabricMC/tiny-remapper@9866789, FabricMC/tiny-remapper@7e4f744 and FabricMC/tiny-remapper@c97e001

--- src/main/java/net/fabricmc/tinyremapper/TinyUtils.java
+++ src/main/java/net/fabricmc/tinyremapper/TinyUtils.java
@@ -32,6 +32,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.BiConsumer;
@@ -239,7 +240,7 @@ public final class TinyUtils {
 
 			readV1(reader, fromIndex, toIndex, classMappingConsumer, fieldMappingConsumer, methodMappingConsumer);
 		} else if (headerLine.startsWith("tiny\t2\t")) {
-			throw new UnsupportedOperationException("Tiny V2 support coming soon");
+			readV2(reader, from, to, headerLine, classMappingConsumer, fieldMappingConsumer, methodMappingConsumer, localMappingConsumer);
 		} else {
 			throw new IOException("Invalid mapping version!");
 		}
@@ -293,4 +294,191 @@ public final class TinyUtils {
 			consumer.accept(mapping, nameTo);
 		}
 	}
+
+	private static void readV2(BufferedReader reader, String from, String to, String headerLine,
+			BiConsumer<String, String> classMappingConsumer,
+			BiConsumer<Mapping, String> fieldMappingConsumer,
+			BiConsumer<Mapping, String> methodMappingConsumer,
+			BiConsumer<Mapping, String[]> localMappingConsumer) throws IOException {
+		String[] parts;
+
+		if (!headerLine.startsWith("tiny\t2\t") || (parts = splitAtTab(headerLine, 0, 5)).length < 5) { //min. tiny + major version + minor version + 2 name spaces
+			throw new IOException("Invalid/unsupported tiny file (incorrect header)");
+		}
+
+		List<String> namespaces = Arrays.asList(parts).subList(3, parts.length);
+		int nsA = namespaces.indexOf(from);
+		int nsB = namespaces.indexOf(to);
+		Map<String, String> obfFrom = nsA != 0 ? new HashMap<>() : null;
+
+		Map<String, String> classes = new HashMap<>();
+		Map<Mapping, String> methods = new HashMap<>();
+		Map<Mapping, String> fields = new HashMap<>();
+		Map<Mapping, String[]> locals = new HashMap<>();
+
+		int partCountHint = 2 + namespaces.size(); // suitable for members, which should be the majority
+		boolean escapedNames = false;
+
+		boolean inHeader = true;
+		boolean inClass = false;
+		boolean inMethod = false;
+
+		String className = null;
+		Mapping member = null;
+
+		int lineNumber = 1;
+		for (String line = reader.readLine(); line != null; line = reader.readLine(), lineNumber++) {
+			if (line.isEmpty()) continue;
+
+			int indent = 0;
+			while (indent < line.length() && line.charAt(indent) == '\t') {
+				indent++;
+			}
+
+			parts = splitAtTab(line, indent, partCountHint);
+			String section = parts[0];
+
+			if (indent == 0) {
+				inHeader = inClass = inMethod = false;
+
+				if ("c".equals(section)) { // class: c <names>...
+					if (parts.length != namespaces.size() + 1) throw new IOException("Invalid class declaration on line " + lineNumber);
+
+					className = unescapeOpt(parts[1 + nsA], escapedNames);
+					String mappedName = unescapeOpt(parts[1 + nsB], escapedNames);
+
+					if (!mappedName.isEmpty()) {
+						classMappingConsumer.accept(className, mappedName);
+						classes.put(className, mappedName);
+						if (obfFrom != null) obfFrom.put(unescapeOpt(parts[1], escapedNames), mappedName);
+					}
+
+					inClass = true;
+				}
+			} else if (indent == 1) {
+				inMethod = false;
+
+				if (inHeader) { // header k/v
+					if ("escaped-names".equals(section)) {
+						escapedNames = true;
+					}
+				} else if (inClass && ("m".equals(section) || "f".equals(section))) { // method/field: m/f <descA> <names>...
+					boolean isMethod = "m".equals(section);
+					if (parts.length != namespaces.size() + 2) throw new IOException("Invalid " + (isMethod ? "metho" : "fiel") + "d declaration on line " + lineNumber);
+
+					String memberDesc = unescapeOpt(parts[1], escapedNames);
+					String memberName = unescapeOpt(parts[2 + nsA], escapedNames);
+					String mappedName = unescapeOpt(parts[2 + nsB], escapedNames);
+					member = new Mapping(className, memberName, memberDesc);
+					inMethod = isMethod;
+
+					if (!mappedName.isEmpty()) (isMethod ? methods : fields).put(member, mappedName);
+				}
+			} else if (indent == 2) {
+				if (inMethod && "p".equals(section)) { // method parameter: p <lv-index> <names>...
+					if (parts.length != namespaces.size() + 2) throw new IOException("Invalid method parameter declaration on line " + lineNumber);
+
+					String mappedName = unescapeOpt(parts[2 + nsB], escapedNames);
+					if (!mappedName.isEmpty()) {
+						int varLvIndex = Integer.parseInt(parts[1]);
+
+						String[] methodLocals = locals.get(member);
+						if (methodLocals == null || methodLocals.length <= varLvIndex) {
+							String[] longerLocals = new String[varLvIndex + 1];
+							if (methodLocals != null) System.arraycopy(methodLocals, 0, longerLocals, 0, methodLocals.length);
+							locals.put(member, methodLocals = longerLocals);
+						}
+
+						assert methodLocals[varLvIndex] == null;
+						methodLocals[varLvIndex] = mappedName;
+					}
+				} else if (inMethod && "v".equals(section)) { // method variable: v <lv-index> <lv-start-offset> <optional-lvt-index> <names>...
+					if (parts.length != namespaces.size() + 4) throw new IOException("Invalid method variable declaration on line " + lineNumber);
+
+					String mappedName = unescapeOpt(parts[4 + nsB], escapedNames);
+					if (!mappedName.isEmpty()) {
+						int varLvIndex = Integer.parseInt(parts[1]);
+						int varStartOpIdx = Integer.parseInt(parts[2]);
+						int varLvtIndex = Integer.parseInt(parts[3]);
+
+						//Don't currently support this as it stands, neither does Yarn so it could be worse
+						throw new UnsupportedOperationException(String.format("%1$s local %2$d: %5$s, start @ %3$d, index %4$d", member, varLvIndex, varStartOpIdx, varLvtIndex, mappedName));
+					}
+				}
+			}
+		}
+
+		UnaryOperator<Mapping> descFixer, methodDescFixer;
+		if (obfFrom != null) {
+			Remapper remapper = new SimpleClassMapper(obfFrom);
+			descFixer = mapping -> new Mapping(mapping.owner, mapping.name, remapper.mapDesc(mapping.desc));
+			methodDescFixer = mapping -> new Mapping(mapping.owner, mapping.name, remapper.mapMethodDesc(mapping.desc));
+		} else {
+			methodDescFixer = descFixer = UnaryOperator.identity();
+		}
+
+		for (Entry<Mapping, String> entry : methods.entrySet()) {
+			methodMappingConsumer.accept(methodDescFixer.apply(entry.getKey()), entry.getValue());
+		}
+		for (Entry<Mapping, String> entry : fields.entrySet()) {
+			fieldMappingConsumer.accept(descFixer.apply(entry.getKey()), entry.getValue());
+		}
+
+		for (Entry<Mapping, String[]> entry : locals.entrySet()) {
+			Mapping mapping = entry.getKey();
+			localMappingConsumer.accept(new Mapping(classes.getOrDefault(mapping.owner, mapping.owner), mapping.name, mapping.desc), entry.getValue());
+		}
+	}
+
+	private static String[] splitAtTab(String s, int offset, int partCountHint) {
+		String[] ret = new String[Math.max(1, partCountHint)];
+		int partCount = 0;
+		int pos;
+
+		while ((pos = s.indexOf('\t', offset)) >= 0) {
+			if (partCount == ret.length) ret = Arrays.copyOf(ret, ret.length * 2);
+			ret[partCount++] = s.substring(offset, pos);
+			offset = pos + 1;
+		}
+
+		if (partCount == ret.length) ret = Arrays.copyOf(ret, ret.length + 1);
+		ret[partCount++] = s.substring(offset);
+
+		return partCount == ret.length ? ret : Arrays.copyOf(ret, partCount);
+	}
+
+	private static String unescapeOpt(String str, boolean escapedNames) {
+		return escapedNames ? unescape(str) : str;
+	}
+
+	private static String unescape(String str) {
+		int pos = str.indexOf('\\');
+		if (pos < 0) return str;
+
+		StringBuilder ret = new StringBuilder(str.length() - 1);
+		int start = 0;
+
+		do {
+			ret.append(str, start, pos);
+			pos++;
+			int type;
+
+			if (pos >= str.length()) {
+				throw new RuntimeException("incomplete escape sequence at the end");
+			} else if ((type = escaped.indexOf(str.charAt(pos))) < 0) {
+				throw new RuntimeException("invalid escape character: \\"+str.charAt(pos));
+			} else {
+				ret.append(toEscape.charAt(type));
+			}
+
+			start = pos + 1;
+		} while ((pos = str.indexOf('\\', start)) >= 0);
+
+		ret.append(str, start, str.length());
+
+		return ret.toString();
+	}
+
+	private static final String toEscape = "\\\n\r\0\t";
+	private static final String escaped = "\\nr0t";
 }
